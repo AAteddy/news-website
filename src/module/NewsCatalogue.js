83d53f23c98bedf9fe2f8c07f0f119aa2399b536
Tedros Alem <tedsaasfaha@gmail.com>
2023-06-18 19:11:22 +0300

    updated the NewsElement.js file and implement the News API integration

---
+++ src/module/NewsCatalogue.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import NewsElement from './NewsElement';
+
+// declaring constant for the news catalogue
+const NewsCatalogue = () => {
+    const [articles, setArticles] = useState([])
+    useEffect(() => {
+        const getArticles = async () => {
+            const response = await axios.get('https://newsapi.org/v2/everything?q=Apple&from=2023-06-18&sortBy=popularity&apiKey=28463e4260e24672ad1f981f9c02e1fb')
+            setArticles(response.data.articles)
+            // Print the data
+            console.log(response)
+        } 
+        getArticles()
+    },[])
+    return (
+        <div>
+            {articles.map(article => {
+                return(
+                    <NewsElement>
+                        
+                    </NewsElement>
+                )
+            })}
+        </div>
+    )
+}
